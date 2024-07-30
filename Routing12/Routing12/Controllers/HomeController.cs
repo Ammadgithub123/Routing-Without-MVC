b205ab2382512e7f9c9145da7375751f07a8d695
MohammadAmmad <117718535+Ammadgithub123@users.noreply.github.com>
2024-07-30 16:53:51 +0500

    Initial commit

---
+++ Routing12/Routing12/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing12.Controllers
+{
+
+    //[Route("Home")]
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        //[Route("Index")]
+        //[Route("[action]")]
+        [Route("~/")]
+        [Route("~/Home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult Data1()
+        //{
+        //    return View("~/Views/Home/Index.cshtml");
+        //}
+
+        //[Route("About")]
+        //[Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //[Route("(Details)/{id?}")]
+        //[Route("[action]/{id?}")]
+         [Route("{id?}")]
+        public int Details(int? id)
+        {
+            return id ?? 1;
+        }
+    }
+}
